bd57a868d8fb8911feb775177465bc4ca2b33557
TJensen-bi <tradsborg89@gmail.com>
2025-10-24 13:16:24 +0200

    Update Basic.py

--- REST_API/Basic.py
+++ REST_API/Basic.py
@@ -1,1 +1,16 @@
+import requests
+import pandas as pd
 
+try:
+    response = requests.get("http://api.openweathermap.org/geo/1.0/reverse?lat=56.075984&lon=12.512430&appid=3db8fc6d42877ba151fe5f6ba8849b0b")
+    response.raise_for_status()  # Vil kaste en HTTPError for dårlige statuskoder
+    data = response.json()
+except requests.exceptions.HTTPError as http_err:
+    print(f"HTTP error occurred: {http_err}")
+except Exception as err:
+    print(f"An error occurred: {err}")
+
+#df = display(data)
+
+# Konverter data til en DataFrame
+df = pd.DataFrame(data)
